fab4011f77d12073292da765b329adf8a03ac54f
Setor Agodo <setor.k.agodo@gmail.com>
2018-05-08 19:40:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
 }
 
 function nowServing(katzDeli){
-  let i = 0;
-  while (i < katzDeli.length){
-    i++;
-  }
 
     if(katzDeli.length === 0) {
     return "There is nobody waiting to be served!";
